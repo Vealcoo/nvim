44e792d0f8225e337916cde38b3deb3fcd4ea1dd
Vealcoo <oouas231556@gmail.com>
2022-10-18 23:06:03 +0800

    update alpha theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,18 +5,55 @@ return require('packer').startup(function()
     "goolord/alpha-nvim",
     config = function ()
 	require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    --     local alpha = require'alpha'
-    --     local dashboard = require'alpha.themes.dashboard'
-    --     dashboard.section.header.val = 'HEADER'
-    --     dashboard.section.buttons.val = {
-    --         dashboard.button( "e", "  New file" , ":ene <BAR> startinsert <CR>"),
-    --         dashboard.button( "q", "  Quit NVIM" , ":qa<CR>"),
-    --     }
+        local alpha = require'alpha'
+        local dashboard = require'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+"            :h-                                  Nhy`               ",
+"           -mh.                           h.    `Ndho               ",
+"           hmh+                          oNm.   oNdhh               ",
+"          `Nmhd`                        /NNmd  /NNhhd               ",
+"          -NNhhy                      `hMNmmm`+NNdhhh               ",
+"          .NNmhhs              ```....`..-:/./mNdhhh+               ",
+"           mNNdhhh-     `.-::///+++////++//:--.`-/sd`               ",
+"           oNNNdhhdo..://++//++++++/+++//++///++/-.`                ",
+"      y.   `mNNNmhhhdy+/++++//+/////++//+++///++////-` `/oos:       ",
+" .    Nmy:  :NNNNmhhhhdy+/++/+++///:.....--:////+++///:.`:s+        ",
+" h-   dNmNmy oNNNNNdhhhhy:/+/+++/-         ---:/+++//++//.`         ",
+" hd+` -NNNy`./dNNNNNhhhh+-://///    -+oo:`  ::-:+////++///:`        ",
+" /Nmhs+oss-:++/dNNNmhho:--::///    /mmmmmo  ../-///++///////.       ",
+"  oNNdhhhhhhhs//osso/:---:::///    /yyyyso  ..o+-//////////:/.      ",
+"   /mNNNmdhhhh/://+///::://////     -:::- ..+sy+:////////::/:/.     ",
+"     /hNNNdhhs--:/+++////++/////.      ..-/yhhs-/////////::/::/`    ",
+"       .ooo+/-::::/+///////++++//-/ossyyhhhhs/:///////:::/::::/:    ",
+"       -///:::::::////++///+++/////:/+ooo+/::///////.::://::---+`   ",
+"       /////+//++++/////+////-..//////////::-:::--`.:///:---:::/:   ",
+"       //+++//++++++////+++///::--                 .::::-------::   ",
+"       :/++++///////////++++//////.                -:/:----::../-   ",
+"       -/++++//++///+//////////////               .::::---:::-.+`   ",
+"       `////////////////////////////:.            --::-----...-/    ",
+"        -///://////////////////////::::-..      :-:-:-..-::.`.+`    ",
+"         :/://///:///::://::://::::::/:::::::-:---::-.-....``/- -   ",
+"           ::::://::://::::::::::::::----------..-:....`.../- -+oo/ ",
+"            -/:::-:::::---://:-::-::::----::---.-.......`-/.      ``",
+"           s-`::--:::------:////----:---.-:::...-.....`./:          ",
+"          yMNy.`::-.--::..-dmmhhhs-..-.-.......`.....-/:`           ",
+"         oMNNNh. `-::--...:NNNdhhh/.--.`..``.......:/-              ",
+"        :dy+:`      .-::-..NNNhhd+``..`...````.-::-`                ",
+"                        .-:mNdhh:.......--::::-`                    ",
+"                           yNh/..------..`                          ",
+"                                                                    ",
+		}
+        dashboard.section.buttons.val = {
+            dashboard.button( "e", "📄 New File" , ":ene <BAR> startinsert <CR>"),
+			dashboard.button( "g", "🔍 Find Files", ":Telescope find_files prompt_prefix=🔍<CR>"),
+			dashboard.button( "g", "🤖 Lazy Git", ":Telescope lazygit<CR>"),
+            dashboard.button( "q", "❌  Quit NVIM" , ":qa<CR>"),
+        }
     --     local handle = io.popen('fortune')
     --     local fortune = handle:read("*a")
     --     handle:close()
     --     dashboard.section.footer.val = fortune
-    --     alpha.setup(dashboard.opts)
+        alpha.setup(dashboard.opts)
     end
 	}
 	-- use "akinsho/bufferline.nvim"
